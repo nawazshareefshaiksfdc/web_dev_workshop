073ed99170ff1f1a6570ff404dd311490e808aa1
nawazshareefshaiksfdc <nawazshareefshaiksfdc@gmail.com>
2024-08-05 18:16:51 +0530

    set-up files for Workshop

---
+++ SlideImages/slideImages.js
@@ -0,0 +1,15 @@
+import bigImage1 from './slideImages/bigImage1.webp'
+import bigImage2 from './slideImages/bigImage2.webp'
+import bigImage3 from './slideImages/bigImage3.webp'
+import bigImage4 from './slideImages/bigImage4.webp'
+import bigImage5 from './slideImages/bigImage5.webp'
+import bigImage6 from './slideImages/bigImage6.webp'
+const slideImages = [
+    { src: bigImage1 },
+    { src: bigImage2 },
+    { src: bigImage3 },
+    { src: bigImage4 },
+    { src: bigImage5 },
+    { src: bigImage6 }
+];
+export default slideImages;
